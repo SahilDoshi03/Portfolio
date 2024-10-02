104c58be876cb87a5f65130144f4efedfd21dd25
Sahil Doshi <sahildoshi@Sahils-MacBook-Pro.local>
2024-10-02 23:49:40 +0530

    Make navbar responsive

--- src/components/composite-components/hero/Hero.tsx
+++ src/components/composite-components/hero/Hero.tsx
@@ -3,23 +3,22 @@ import { BsGithub, BsLinkedin } from "react-icons/bs";
 const Hero = () => {
   return (
     <div id="info" className="flex items-center justify-center md:justify-start md:items-end h-screen text-primaryLight relative">
-
       <section
-        className="flex flex-col gap-4 w-[35rem] md:ml-[10vw] md:mb-[10rem] p-[2.5rem] z-10 bg-secondaryDark rounded-xl 
+        className="flex flex-col gap-[1rem] w-[28rem] lg:w-[35rem] md:ml-[clamp(4rem,10vw,25rem)] md:mb-[clamp(4rem,8vw,10rem)] p-[2.5rem] z-10 bg-secondaryDark rounded-xl 
         bg-clip-padding backdrop-filter backdrop-blur-xl bg-opacity-40"
       >
         <div>
-          <h3 className="text-[1.5rem] text-accent font-[600] tracking-wide">
+          <h3 className="text-[1.2rem] lg:text-[1.5rem] text-accent font-[600] tracking-wide">
             Hello World!
           </h3>
-          <h2 className="text-[3rem] tracking-wide font-[300]">
+          <h2 className="text-[2.4rem] lg:text-[3rem] tracking-wide font-[300]">
             I'm Sahil Doshi
           </h2>
-          <h3 className="text-[1.5rem] tracking-wide font-[100]">
+          <h3 className="text-[1.2rem] lg:text-[1.5rem] tracking-wide font-[100]">
             Full Stack Developer
           </h3>
         </div>
-        <p className="text-[1rem] text-justify tracking-wide font-[100]">
+        <p className="text-[.9rem] lg:text-[1rem] text-justify tracking-wide font-[100]">
           I’m a versatile software developer based in Mumbai, India, with
           extensive expertise in MERN stack development. Check out my projects
           to see how I transform concepts into engaging applications. Let’s
--- src/components/composite-components/navbar/Navbar.tsx
+++ src/components/composite-components/navbar/Navbar.tsx
@@ -4,62 +4,118 @@ import { useState, useEffect } from "react";
 import Link from "next/link";
 
 const Navbar = () => {
-    const [active, setActive] = useState("");
-    const [toggle, setToggle] = useState(false);
-    const [navbarColor, setNavbarColor] = useState("bg-transparent");
+  const [active, setActive] = useState(false);
+  const [navbarColor, setNavbarColor] = useState("bg-transparent");
 
-    useEffect(() => {
-        const handleScroll = () => {
-            const scrollY = window.scrollY;
-            if (scrollY > 50) {
-                setNavbarColor("bg-primaryDark");
-            } else {
-                setNavbarColor("bg-transparent");
-            }
-        };
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollY = window.scrollY;
+      if (scrollY > 50) {
+        setNavbarColor("bg-primaryDark");
+      } else {
+        setNavbarColor("bg-transparent");
+      }
+    };
 
-        window.addEventListener("scroll", handleScroll);
-        return () => {
-            window.removeEventListener("scroll", handleScroll);
-        };
-    }, []);
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
-    return (
-        <nav
-            className={`${navbarColor} _container_padding_x fixed w-screen flex items-center justify-between py-[.5rem] text-white z-50 transition-colors duration-500`}
-        >
-            {/* logo */}
-            <div className="flex items-center gap-[4vw]">
-                <div className="flex items-center gap-3">
-                    <span className="text-[3rem] tracking-wide font-[500]">
-                        <Link href="#info">Sahil</Link>
-                    </span>
-                    <div className="w-4 h-4 bg-accent rounded-full translate-y-2"></div>
-                </div>
-                {/* nav buttons */}
-                <ul className="_nav_text flex gap-10">
-                    <li>
-                        <Link href="#info">Info</Link>
-                    </li>
-                    <li>
-                        <Link href="#skills">Skills</Link>
-                    </li>
-                    <li>
-                        <Link href="#projects">Projects</Link>
-                    </li>
-                    <li>
-                        <Link href="#contact">Contact</Link>
-                    </li>
-                </ul>
-            </div>
-            {/* dark mode switch  and resume*/}
-            <button className="py-3 px-4 rounded-xl text-[1.2rem] text-accent font-[500] tracking-wide cursor-pointer bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-10 bg-primaryDark">
-                <Link href="/resume.pdf" download>
-                    Resume
-                </Link>
-            </button>
-        </nav>
-    );
+  return (
+    <nav
+      className={`flex flex-col md:flex-row md:items-center md:gap-8 ${navbarColor} ${active ? "!bg-primaryDark" : ""} _container_padding_x fixed w-screen z-50`}
+    >
+      <div className="flex items-center max-md:gap-8">
+        <div className="flex items-center gap-8">
+          <div
+            onClick={() => {
+              setActive((prev) => !prev);
+            }}
+            className="w-8 h-8 md:hidden flex flex-col items-center justify-center gap-[.5rem] relative"
+          >
+            <div
+              className={`w-7 h-[2px] bg-white rounded-full transition-transform duration-300 ease-in-out ${active ? "absolute rotate-45" : ""}`}
+            ></div>
+            <div
+              className={`w-7 h-[2px] bg-white rounded-full transition-transform duration-300 ease-in-out ${active ? "absolute -rotate-45 " : ""}`}
+            ></div>
+          </div>
+        </div>
+
+        {/* logo */}
+        <div className="flex items-center gap-3">
+          <span className="text-white text-[3rem] tracking-wide font-[500]">
+            <Link href="#info">Sahil</Link>
+          </span>
+          <div className="w-4 h-4 bg-accent rounded-full translate-y-2"></div>
+        </div>
+      </div>
+      <div
+        className={`${active ? "" : "max-md:hidden"} w-full flex flex-col md:flex-row md:items-center justify-between py-[.5rem] text-white transition-all duration-500`}
+      >
+        <ul className="_nav_text flex flex-col md:flex-row gap-8 mt-2 mb-8 md:mt-0 md:mb-0">
+          <li>
+            <Link
+              onClick={() => {
+                setActive((prev) => !prev);
+              }}
+              href="#info"
+            >
+              Info
+            </Link>
+          </li>
+          <li>
+            <Link
+              onClick={() => {
+                setActive((prev) => !prev);
+              }}
+              href="#skills"
+            >
+              Skills
+            </Link>
+          </li>
+          <li>
+            <Link
+              onClick={() => {
+                setActive((prev) => !prev);
+              }}
+              href="#projects"
+            >
+              Projects
+            </Link>
+          </li>
+          <li>
+            <Link
+              onClick={() => {
+                setActive((prev) => !prev);
+              }}
+              href="#contact"
+            >
+              Contact
+            </Link>
+          </li>
+          <li className="md:hidden">
+            <Link
+              onClick={() => {
+                setActive((prev) => !prev);
+              }}
+              href="/resume.pdf"
+              download
+            >
+              Resume
+            </Link>
+          </li>
+        </ul>
+        <button className="hidden md:inline-block py-3 px-4 rounded-xl text-[1.2rem] text-accent font-[500] tracking-wide cursor-pointer bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-10 bg-primaryDark">
+          <Link href="/resume.pdf" download>
+            Resume
+          </Link>
+        </button>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
