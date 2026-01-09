899cf5def24364a53ff2ae937673dc84aa9c3ad9
Sahil Doshi <sahildsh03@gmail.com>
2026-01-09 09:53:35 +0530

    update

--- src/components/core-components/skill-component/skill-component.tsx
+++ src/components/core-components/skill-component/skill-component.tsx
@@ -17,15 +17,15 @@ export const SkillComponent = ({
   stack,
 }: SkillComponentProps) => {
   const numberOfLines = 60;
-  const radius = 150;
+  const radius = 100; // Reduced from 150
 
   const lines = Array.from({ length: numberOfLines }).map((_, i) => {
     const angle = -135 + (i * 270) / numberOfLines;
     const lineStyle: CSSProperties = {
       transform: `rotate(${angle}deg)`,
       position: "absolute",
-      width: "2px",
-      height: "20px",
+      width: "1.5px", // Reduced from 2px
+      height: "12px", // Reduced from 20px
       backgroundColor: `${i < (value / 100) * numberOfLines ? "#3FB4B1" : "white"}`,
       transformOrigin: `0 ${radius}px`,
     };
@@ -34,16 +34,16 @@ export const SkillComponent = ({
   });
 
   return (
-    <div className="relative w-[300px] h-[300px]">
+    <div className="relative w-[200px] h-[200px]"> {/* Reduced from 300px */}
       <div className="absolute left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center">
         {lines}
       </div>
       <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-        <Image src={img} width={width} alt="skill-logo" />
+        <Image src={img} width={width * 0.6} alt="skill-logo" /> {/* Scaled down image */}
       </div>
-      <div className="flex flex-col items-center justify-center absolute bottom-[1rem] left-1/2 -translate-x-1/2">
-        <div className="text-white text-[1.2em] font-light">{value}%</div>
-        <div className="text-[1.2rem] font-light tracking-wider">{stack}</div>
+      <div className="flex flex-col items-center justify-center absolute bottom-[0.5rem] left-1/2 -translate-x-1/2">
+        <div className="text-white text-[0.9em] font-light">{value}%</div>
+        <div className="text-[0.9rem] font-light tracking-wider">{stack}</div>
       </div>
     </div>
   );
